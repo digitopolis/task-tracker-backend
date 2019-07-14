ffac48306b1f4b2227b9dcaedec48b17afcc03ad
Matt Readout <matt.readout@gmail.com>
2019-07-13 22:15:55 -0400

    define user model, controller, routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+	before_action :find_user, only: [:show]
+
+	def index
+	  @users = User.all
+		render json: @users
+	end
+
+	def show
+	  render json: @user
+	end
+
+	private
+
+	def user_params
+	  params.require(:user).permit(:name, :username, :password)
+	end
+
+	def find_user
+	  @user = User.find(params[:id])
+	end
 end
