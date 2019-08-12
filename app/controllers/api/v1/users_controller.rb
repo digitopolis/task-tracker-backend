b664e8e9dd65c6ce2f37c29c634a51b9e7813452
Matt Readout <matt.readout@gmail.com>
2019-08-11 21:55:05 -0400

    users controller #create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,16 @@ class Api::V1::UsersController < ApplicationController
 	  @user = User.new
 	end
 
+	def create
+	  @user = User.create(user_params)
+		if @user.valid?
+			@token = encode_token(user_id: @user.id)
+			render json: { user: UserSerializer(@user), jwt: @token }, status: :created
+		else
+			render json: { error: 'failed to register new user' }, status: :not_acceptable
+		end
+	end
+
 	private
 
 	def user_params
