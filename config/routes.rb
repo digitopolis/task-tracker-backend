5c5fbd18ed253bda66bc3adb4b649ef8c8790106
Matt Readout <matt.readout@gmail.com>
2019-08-18 22:48:52 -0400

    add routes for #new and #create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
-			resources :users, only: [:index, :show]
+			resources :users, only: [:index, :show, :new, :create]
 		end
 	end
 end
